d045bda9d4a541dd477c2e3f203b539fe1a0ea3c
acer36627 <79729049+acer36627@users.noreply.github.com>
2022-02-17 19:57:05 -0700

    Continued work on GUI
    Added more to main menu, completed a barebones pause menu, and will start work on the gameboard.

--- SnakesAndLadders/src/com/company/MainWindow.java
+++ SnakesAndLadders/src/com/company/MainWindow.java
@@ -6,8 +6,11 @@ public class MainWindow {
     private JPanel panel1;
     private JLabel snlLogo;
     private JButton btnStart;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField playername1;
+    private JTextField playername2;
+    private JTextField playername3;
+    private JTextField playername4;
+    private JButton btnExit;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
