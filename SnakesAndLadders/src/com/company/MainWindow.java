1c3e572ac9ec6133253030353bcad93908d724b2
acer36627 <79729049+acer36627@users.noreply.github.com>
2022-02-23 16:22:07 -0700

    Started on main board gui

--- SnakesAndLadders/src/com/company/MainWindow.java
+++ SnakesAndLadders/src/com/company/MainWindow.java
@@ -7,10 +7,9 @@ public class MainWindow {
     private JLabel snlLogo;
     private JButton btnStart;
     private JTextField playername1;
-    private JTextField playername2;
-    private JTextField playername3;
-    private JTextField playername4;
     private JButton btnExit;
+    private JLabel board;
+    private JButton button1;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
